e73af2bfbc7e1a81c6d807e6edb9d4bdebad3c14
possibilliontech <ajay.gadadasu@possibilliontech.com>
2017-07-19 17:00:42 +0530

    ravi

--- app/src/main/java/com/buzzed/MainActivity.java
+++ app/src/main/java/com/buzzed/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Dialog dialog = new Dialog(this);
-//        dialog.show();
+        Dialog dialog = new Dialog(this);
+        dialog.show();
     }
 }
 
